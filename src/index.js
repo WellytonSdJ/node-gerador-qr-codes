c8f7d03ad5ae1d7b8624b1ab78069749746fd262
Wellyton <wellytonsdj@gmail.com>
2025-01-20 18:54:03 -0300

    qrcode funcional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('running')
+import prompt from "prompt";
+import promptMain from './prompts/prompt-main.js'
+import createQRCode from "./services/qr-code/create.js";
+
+(async function main () {
+  prompt.get(promptMain, async (err, choose)  => {
+    if(choose.select == 1) await createQRCode()
+    if(choose.select == 2) console.log('ESCOLHEU PASSWORD')      
+  })
+})()
---
+++ src/prompts/prompt-qrcode.js
@@ -0,0 +1,24 @@
+import chalk from "chalk"
+
+const promptQRCode = [
+  {
+    name: "link",
+    description: chalk.yellow("Digite o link para gerar o QR CODE")
+  },
+  {
+    name: "type",
+    description: chalk.yellow("Escolha entre o tipo de QRCode (1 - NORMAL ou (2 - TERMINAL"),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    required: true
+  },
+  {
+    name: "qrcode",
+    message: chalk.yellow.bold('Name must be only letters, spaces, or dashes'),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    required: true
+  }
+]
+
+export default promptQRCode
