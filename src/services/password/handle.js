1c2485a79925116498092d3b3b9dbca4bb9a66ae
Wellyton <wellytonsdj@gmail.com>
2025-02-10 17:55:54 -0300

    refactor melhoria de utilidades

--- src/services/password/handle.js
+++ src/services/password/handle.js
@@ -1,36 +1,13 @@
-async function allowedCharacters(){
-  let allowed = []
-
-  if (process.env.UPPERCASE_LETTERS === "true") {
-    allowed.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-  }
-
-  if(process.env.LOWERCASE_LETTERS) {
-    allowed.push(..."abcdefghijklmnopqrstuvwxyz")
-  }
-
-  if(process.env.NUMBERS === "true") {
-    allowed.push(..."0123456789")
-  }
-
-  if(process.env.SPECIAL_CHARACTERS === "true") {
-    allowed.push(..."0123456789")
-  }
-
-  return allowed
-}
-
+import allowedCharacters from './utils/allowed-caracters.js'
 async function handle() {
-  let characters = [] // inicia vazio e depois será checado no .env o que será aceito ou não
+  let characters = []
   let password = ""
 
   const passwordLength = process.env.PASSWORD_LENGTH
   characters = await allowedCharacters()
 
-  
-
-  for(let i = 0; i < passwordLength; i++) {
-    const index = Math.floor(Math.random() * characters.length) // sorteia valor aleatório
+  for (let i = 0; i < passwordLength; i++) {
+    const index = Math.floor(Math.random() * characters.length)
     password += characters[index]
   }
 
