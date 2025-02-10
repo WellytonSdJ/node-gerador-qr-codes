84d44158709a9d2fcd25f85717a8f77b6bc5b0ba
Wellyton <wellytonsdj@gmail.com>
2025-02-10 17:49:31 -0300

    gerador de password funcional

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import prompt from "prompt";
 import promptMain from './prompts/prompt-main.js'
+
 import createQRCode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
 
 (async function main () {
   prompt.get(promptMain, async (err, choose)  => {
     if(choose.select == 1) await createQRCode()
-    if(choose.select == 2) console.log('ESCOLHEU PASSWORD')      
+    if(choose.select == 2) await createPassword()
   })
 })()
